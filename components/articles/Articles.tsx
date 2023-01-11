07084730152d3784883be94f48f503f5306a45f4
Bartosz Czaplicki <h3ronnn@gmail.com>
2023-01-11 20:02:18 +0100

    Dodanie wyswietlania artykulow z bazy danych

--- components/articles/Articles.tsx
+++ components/articles/Articles.tsx
@@ -1,12 +1,11 @@
 import styles from "./articles.module.scss";
-import { articles } from "./content";
 
-const Articles = () => {
+const Articles = ({ articles }: any) => {
   return (
     <div className={styles.wrapper}>
       <ul>
-        {articles.map(({ id, title, content }) => (
-          <li key={id}>
+        {articles.map(({ id, title, content }: any) => (
+          <li key={id} className={styles.article}>
             <h2>{title}</h2>
             <p>{content}</p>
           </li>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
+import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
-    </div>
+    </SessionProvider>
   );
 }
