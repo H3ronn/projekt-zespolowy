2518f9c1ca74c9def3aa1f4bd17c1dc3501f35de
Bartosz Czaplicki <h3ronnn@gmail.com>
2023-01-12 20:13:57 +0100

    Modyfikacja komponentu Header.tsx

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,13 +1,36 @@
 import Link from "next/link";
 import styles from "./header.module.scss";
+import { signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data: session, status } = useSession();
+  console.log(session);
+  console.log(status);
+
+  if (status === "loading") {
+    return <div>Loading</div>;
+  }
+
   return (
     <div className={styles.wrapper}>
+      <div>
+        {!!session && (
+          <div>
+            <button className={styles.logOut} onClick={() => signOut()}>
+              Wyloguj ({session.user?.email})
+            </button>
+          </div>
+        )}
+      </div>
       <h1 className={styles.heading}>Serwis informacyjny</h1>
-      <Link href="/login" className={styles.link}>
-        Zaloguj
+      <Link href="/api/auth/signin" className={styles.link}>
+        Log in
       </Link>
+      {!!session && (
+        <Link href="/add" className={styles.addArticle}>
+          Dodaj artykuł
+        </Link>
+      )}
     </div>
   );
 };
